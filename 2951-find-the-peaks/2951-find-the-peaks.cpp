8748781a5a93222e81fbd01be581ce357209f5ba
Tarun <tarun.337799@gmail.com>
2026-07-06 02:42:19 +0530

    Time: 0 ms (100%), Space: 24.6 MB (7.63%) - LeetHub

--- 2951-find-the-peaks/2951-find-the-peaks.cpp
+++ 2951-find-the-peaks/2951-find-the-peaks.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     vector<int> findPeaks(vector<int>& mountain) {
         vector<int> peaks;
-        for(int i = 0 ; i < mountain.size() ; i ++ ){
+        int n = mountain.size();
+        for(int i = 0 ; i < n ; i ++ ){
             if (i==0 or mountain[i] <= mountain[i-1]) continue;
-            if (i==mountain.size()-1 or mountain[i] <= mountain[i+1]) continue;
+            if (i==n-1 or mountain[i] <= mountain[i+1]) continue;
             peaks.push_back(i);
         }
         return peaks;
