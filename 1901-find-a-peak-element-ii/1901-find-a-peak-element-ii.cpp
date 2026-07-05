200fd9ed4e47392b3e11d4ba6acdd0c290c465d0
Tarun <tarun.337799@gmail.com>
2026-07-06 02:29:46 +0530

    Time: 4 ms (6.08%), Space: 50.2 MB (68.94%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int a = 0;
+        int b = 0;
         for (int i = 0 ; i < mat.size() ; i ++ ){
             for (int j = 0 ; j < mat[0].size() ; j ++ ){
-                if (j>0) if(mat[i][j] < mat[i][j-1]) continue;
-                if (i>0) if(mat[i][j] < mat[i-1][j]) continue;
-                if (j!=mat[0].size()-1) if(mat[i][j] < mat[i][j+1]) continue;
-                if (i!=mat.size()-1) if(mat[i][j] < mat[i+1][j]) continue;
-                return {i,j};
+                if (mat[i][j] > mat[a][b]){
+                    a = i;
+                    b = j;
+                } 
             }
         }
-        return {-1,-1};
+        return {a,b};
     }
 };
