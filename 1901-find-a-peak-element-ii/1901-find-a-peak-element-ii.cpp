9685618efa280bd51190b65766da76370981ebf6
Tarun <tarun.337799@gmail.com>
2026-07-06 03:42:24 +0530

    Time: 0 ms (100%), Space: 50.2 MB (49.54%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        int i = 0;
-        int j = 0;
-        while(1){
-            if(i!=0 and mat[i][j] < mat[i-1][j]){
-                i--;
-                continue;
+        int n = mat.size();
+        int m = mat[0].size();
+        int low = 0;
+        int high = n-1;
+        while(low <= high){
+            int mid1 = low + (high-low)/2;
+            int mid2 = 0;
+            for(int j = 0 ; j < m ; j++ ){
+                if (mat[mid1][mid2]<mat[mid1][j]) mid2 = j;
             }
-            if(i!=mat.size()-1 and mat[i][j] < mat[i+1][j]){
-                i++;
-                continue;
-            }
-            if(j!=0 and mat[i][j] < mat[i][j-1]){
-                j--;
-                continue;
-            }
-            if(j!=mat[0].size()-1 and mat[i][j] < mat[i][j+1]){
-                j++;
-                continue;
-            }
-
-            return {i,j};
+            if((mid1==0 or mat[mid1][mid2]>mat[mid1-1][mid2]) and (mid1==n-1 or mat[mid1][mid2]>mat[mid1+1][mid2])) return {mid1,mid2};
+            else if (mid1==n-1 or mat[mid1][mid2]<mat[mid1+1][mid2]) low = mid1+1;
+            else high = mid1-1;
         }
-        return {i,j};
+        return {-1,-1};
     }
 };
