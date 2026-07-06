474e78cc1a2ea5fba65a4f01fb97a0ba88fb8d84
Tarun <tarun.337799@gmail.com>
2026-07-06 10:31:19 +0530

    Time: 48 ms (21.1%), Space: 105.8 MB (63.36%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    bool freq(vector<int>& nums, int k, int mid){
-        vector<long long> cumu;
-        long long gum = 0;
-        for(int i = 0 ; i < nums.size() ; i ++ ){
-            gum+=nums[i];
-            cumu.push_back(gum);
-        }
-        int left = nums.size()-mid;
-        int right = nums.size()-1;
-        while(left>=0){
-            long long sum = cumu[right];
-            if (left > 0) sum -= cumu[left - 1];
-            sum = (long long)nums[right] * mid - sum;
-            if (sum <= k) return true;
-            left--;
-            right--;
-        }
-        return false;
-    }
     int maxFrequency(vector<int>& nums, int k) {
-        int low = 1;
-        int high = nums.size();
         sort(nums.begin(),nums.end());
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if (!freq(nums,k,mid)) high = mid-1;
-            else low = mid+1;
+        if (nums.size()<2) return nums.size();
+        if (nums.size()==2) {
+            if(nums[1]-nums[0]>k) return 1;
+            return 2;
         }
-        return high;
+        int qax = 1;
+        int left = nums.size()-2;
+        int right = nums.size()-1;
+        long long sum = nums[right]-nums[left];
+        while (left > 0 and left <= right){
+            int size = right-left+1;
+            if(sum <= k){
+                left--;
+                sum+=nums[right]-nums[left];
+                qax = max(qax,size);
+            }
+            else{
+                sum-=(nums[right]-nums[right-1])*(size-1);
+                right--;
+            }
+        }
+        if (sum <= k) qax = max(qax, right - left + 1);
+        return qax;
     }
 };
