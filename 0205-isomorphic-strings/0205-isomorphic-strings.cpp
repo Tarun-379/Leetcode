7fbd0dbeb22bef5086725994d3d323cebc045497
Tarun <tarun.337799@gmail.com>
2026-07-06 12:47:55 +0530

    Time: 2 ms (49.08%), Space: 9.5 MB (28.94%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -5,14 +5,10 @@ public:
         map<char,char> mp1;
         map<char,char> mp2;
         for(int i = 0 ; i < s.size() ; i ++ ){
-            if (!mp1.count(s[i]))
-                mp1[s[i]] = t[i];
-            else if (mp1[s[i]] != t[i])
-                return false;
-            if (!mp2.count(t[i]))
-                mp2[t[i]] = s[i];
-            else if (mp2[t[i]] != s[i])
-                return false;
+            if (!mp1.count(s[i])) mp1[s[i]] = t[i];
+            else if (mp1[s[i]] != t[i]) return false;
+            if (!mp2.count(t[i])) mp2[t[i]] = s[i];
+            else if (mp2[t[i]] != s[i]) return false;
         }
         return true;
     }
