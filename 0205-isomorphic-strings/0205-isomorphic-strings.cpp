7b69278c0ffc66cccd0ab7ef5ea6c8afee86d49b
Tarun <tarun.337799@gmail.com>
2026-07-06 12:47:13 +0530

    Time: 2 ms (49.08%), Space: 9.4 MB (28.94%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        map<char,char> mp1;
+        map<char,char> mp2;
+        for(int i = 0 ; i < s.size() ; i ++ ){
+            if (!mp1.count(s[i]))
+                mp1[s[i]] = t[i];
+            else if (mp1[s[i]] != t[i])
+                return false;
+            if (!mp2.count(t[i]))
+                mp2[t[i]] = s[i];
+            else if (mp2[t[i]] != s[i])
+                return false;
+        }
+        return true;
+    }
+};
