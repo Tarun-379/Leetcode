c592cac893e0d825dcf911f2d9171012a9067059
Tarun <tarun.337799@gmail.com>
2026-07-15 07:04:59 +0530

    Time: 0 ms (100%), Space: 127.2 MB (27.44%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -19,11 +19,10 @@ public:
             temp = temp->next;
         }
         vector<int> st;
-        bool csk = true;
         int i = 0 ;
         while(head!=NULL){
             if(i < s/2) st.push_back(head->val);
-            else if(s%2!=0 and i == (s/2) and csk) csk = false;
+            else if(s%2!=0 and i == s/2) ;
             else {
                 if(head->val != st.back()) return false;
                 st.pop_back();
