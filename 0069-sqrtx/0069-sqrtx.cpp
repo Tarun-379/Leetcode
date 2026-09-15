a01df5f1395a7db6f291eeac2df11a706445bab0
Tarun <tarun.337799@gmail.com>
2026-09-16 04:44:54 +0530

    Time: 0 ms (100%), Space: 8.4 MB (86.99%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -2,10 +2,16 @@ class Solution {
 public:
     int mySqrt(int x) {
         if(x<2) return x;
-        for(long long i = 0 ; i <= x ; i ++ ){
-            if( (long long)i*i == x ) return i;
-            else if(i*i > x) return i-1;
+        int left = 1 , right = x/2;
+        int ret = 1;
+        while(left<=right){
+            long long mid = left+(right-left)/2;
+            if(mid*mid <= x){
+                ret = mid;
+                left = mid+1;
+            }
+            else right = mid-1;
         }
-        return 0;
+        return ret;
     }
 };
