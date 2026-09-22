35e782b4d1c18ad385d2c952e5b5a06b14b658aa
Tarun <tarun.337799@gmail.com>
2026-09-23 05:23:50 +0530

    Time: 0 ms (100%), Space: 25.9 MB (48.28%) - LeetHub

--- 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.cpp
+++ 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.cpp
@@ -3,13 +3,7 @@ public:
     int sumIndicesWithKSetBits(vector<int>& nums, int k) {
         int ret = 0;
         for(int i = 0 ; i < nums.size() ; i ++ ){
-            int count = 0;
-            int temp = i;
-            while(temp){
-                count+=temp&1;
-                temp>>=1;
-            }
-            if(count==k) ret+=nums[i];
+            if(__builtin_popcount(i) == k) ret+=nums[i];
         }
         return ret;
     }
